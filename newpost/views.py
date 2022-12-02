c3fc581c71b436284c150e81e17dc8ad45292b58
aviiciii <laavesh1@gmail.com>
2022-12-02 11:05:45 +0530

    newpost page ui completed

--- newpost/views.py
+++ newpost/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def newpost(request):
+    if request.method == 'POST':
+        post = request.POST['post']
+        print(post)
     return render(request, "newpost/newpost.html")
